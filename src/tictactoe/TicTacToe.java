482729f4506b77dd331080b142ace2d3bdebf798
TNT-Hou <minhou@tntsb.com>
2015-11-05 11:33:42 +0800

    Simplify code
    - create buttons using array and for loop instead of one by one
    - check winner using multidimensional array and for loop

--- src/tictactoe/TicTacToe.java
+++ src/tictactoe/TicTacToe.java
@@ -1,6 +1,5 @@
 package tictactoe;
 
-import java.awt.Color;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -13,15 +12,12 @@ public class TicTacToe implements ActionListener{
 	
 	// Instance Variables
 	private JFrame window = new JFrame("Tic-Tac-Toe");
-	private JButton button1 = new JButton("");
-	private JButton button2 = new JButton("");
-	private JButton button3 = new JButton("");
-	private JButton button4 = new JButton("");
-	private JButton button5 = new JButton("");
-	private JButton button6 = new JButton("");
-	private JButton button7 = new JButton("");
-	private JButton button8 = new JButton("");
-	private JButton button9 = new JButton("");
+	private int[][] winCombinations = new int [][]{   //2D arrays: int[7][3]
+			{0, 1, 2}, {3, 4, 5}, {6, 7, 8},  // horizontal
+			{0, 3, 6}, {1, 4, 7}, {2, 5, 8},  // vertical
+			{0, 4, 8}, {2, 4, 6}  			  // diagonal
+	};
+	private JButton buttons[] = new JButton[9];
 	private String letter = "";
 	private int count = 0;
 	private boolean win = false;
@@ -36,71 +32,42 @@ public class TicTacToe implements ActionListener{
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setLayout(new GridLayout(3,3));
 		
-		//add buttons to window
-		window.add(button1);
-		window.add(button2);
-		window.add(button3);
-		window.add(button4);
-		window.add(button5);
-		window.add(button6);
-		window.add(button7);
-		window.add(button8);
-		window.add(button9);
-		
-		//add action listeners to buttons
-		button1.addActionListener(this);
-		button2.addActionListener(this);
-		button3.addActionListener(this);
-		button4.addActionListener(this);
-		button5.addActionListener(this);
-		button6.addActionListener(this);
-		button7.addActionListener(this);
-		button8.addActionListener(this);
-		button9.addActionListener(this);
+		//add buttons to window and action listeners to buttons
+		for(int i = 0; i <= 8; i++){
+			buttons[i]= new JButton();
+			window.add(buttons[i]);
+			buttons[i].addActionListener(this);
+		}
 		
 		//show the window
 		window.setVisible(true);
 	}
 	
 	public String getTurn(){
-		if (count == 1 || count == 3 || count == 5 || count == 7 || count == 9)
+		if (count %2 != 0)
 			return "X";
-		else if (count == 2 || count == 4 || count == 6 || count == 8 || count == 10)
+		else
 			return "O";
-		else 
-			return "";
 	}
 	
 	public void checkWinner(){
-		// horizontal
-		if (button1.getText() == button2.getText() && button2.getText() == button3.getText() && button1.getText() != "")
-			win = true;
-		else if (button4.getText() == button5.getText() && button5.getText() == button6.getText() && button4.getText() != "")
-			win = true;
-		else if (button7.getText() == button8.getText() && button8.getText() == button9.getText() && button7.getText() != "")
-			win = true;
-		
-		// vertical
-		else if (button1.getText() == button4.getText() && button4.getText() == button7.getText() && button1.getText() != "")
-			win = true;
-		else if (button2.getText() == button5.getText() && button5.getText() == button8.getText() && button2.getText() != "")
-			win = true;
-		else if (button3.getText() == button6.getText() && button6.getText() == button9.getText() && button3.getText() != "")
-			win = true;
-		
-		// diagonal
-		else if (button1.getText() == button5.getText() && button5.getText() == button9.getText() && button1.getText() != "")
-			win = true;
-		else if (button3.getText() == button5.getText() && button5.getText() == button7.getText() && button3.getText() != "")
-			win = true;
-		else
-			win = false;
+		for(int i=0; i<=7; i++){
+			if (buttons[winCombinations[i][0]].getText().equals(
+					buttons[winCombinations[i][1]].getText())
+					&& buttons[winCombinations[i][1]].getText().equals(
+							buttons[winCombinations[i][2]].getText())
+					&& buttons[winCombinations[i][0]].getText() != ""){
+				win = true;
+			}
+		}
 		
 		if (win == true){
-			JOptionPane.showMessageDialog(null, letter + " Won!");
+			JOptionPane.showMessageDialog(null, letter + " WON!");
+			System.exit(0);
 		}
 		else if (count == 9 && win == false){
-			JOptionPane.showMessageDialog(null, "Tie game!");
+			JOptionPane.showMessageDialog(null, "TIE GAME!");
+			System.exit(0);
 		}
 	}
 	
@@ -109,44 +76,10 @@ public class TicTacToe implements ActionListener{
 	public void actionPerformed(ActionEvent a) {
 		count++;
 		letter = getTurn();
-		if (a.getSource() == button1){
-			button1.setText(letter);
-			button1.setEnabled(false);
-		}
-		else if (a.getSource() == button2){
-			button2.setText(letter);
-			button2.setEnabled(false);
-		}
-		else if (a.getSource() == button3){
-			button3.setText(letter);
-			button3.setEnabled(false);
-		}
-		else if (a.getSource() == button4){
-			button4.setText(letter);
-			button4.setEnabled(false);
-		}
-		else if (a.getSource() == button5){
-			button5.setText(letter);
-			button5.setEnabled(false);
-		}
-		else if (a.getSource() == button6){
-			button6.setText(letter);
-			button6.setEnabled(false);
-		}
-		else if (a.getSource() == button7){
-			button7.setText(letter);
-			button7.setEnabled(false);
-		}
-		else if (a.getSource() == button8){
-			button8.setText(letter);
-			button8.setEnabled(false);
-		}
-		else if (a.getSource() == button9){
-			button9.setText(letter);
-			button9.setEnabled(false);
-		}
+		JButton pressedButton = (JButton) a.getSource();
+		pressedButton.setText(letter);
+		pressedButton.setEnabled(false);
 		checkWinner();
-		
 	}
 
 }
